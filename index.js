470ffd50374839ff4ff8ac033638f3dc546eddd6
GladysKigen <gladys.kigen@student.moringa.com>
2022-12-03 12:53:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+//let input;
+//input =document.getElementById('input'); 
+//const sinon = require('sinon');
+//nsinon.spy(input,'addEventListener');
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+     
 }
